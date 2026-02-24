1505d10d659095167faf13fbad6c44d8f33da172
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2026-02-24 13:58:25 -0500

    redo the number of channels for training

--- model/train.py
+++ model/train.py
@@ -74,7 +74,7 @@ def train():
     print("Training Attention U-Net")
     print("==============================")
 
-    model = AttentionUNet(in_channels=11, out_channels=1).to(cfg.device)
+    model = AttentionUNet(in_channels=17, out_channels=1).to(cfg.device)
     criterion = nn.L1Loss()   # Better for SWE / regression problems
     optimizer = optim.Adam(model.parameters(), lr=cfg.lr)
 
