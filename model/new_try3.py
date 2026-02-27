c6fb829d7f50b86367d29ecaae06012f1c92110f
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2026-02-27 12:10:09 -0500

    test

--- model/new_try3.py
+++ model/new_try3.py
@@ -1070,17 +1070,17 @@ def main():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     
     #IPython.embed()
-    num_epochs = 10 #10 #1000
+    num_epochs = 30 #10 #1000
     batch_size = 16
-    learning_rate = 1e-5 #0.01 ### learning rate start it really small? it will take longer to learn though 
-    patience = 10 #400
+    learning_rate = 1e-6 #0.01 ### learning rate start it really small? it will take longer to learn though 
+    patience = 5 #400
     
     # Patching config
     patch_size = 256 #128
     stride = int(patch_size/ 2) #64  # 50% overlap
     min_valid_fraction = 0.3  # Skip patches with <30% valid pixels
     
-    checkpoint_dir = "./checkpoints_elevPM_NDSI_CC_1e-5_ps256_W_smoothL1loss"
+    checkpoint_dir = "./checkpoints_elevPM_NDSI_CC_1e-6_ps256_W_smoothL1loss"
     os.makedirs(checkpoint_dir, exist_ok=True)
     
     # Load FULL zarr files
