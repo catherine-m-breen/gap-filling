a868b947fe301248ffa978ec80fa18c3f970e51a
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2026-03-04 16:11:53 -0500

    test

--- model/ablation_study2.py
+++ model/ablation_study2.py
@@ -941,7 +941,16 @@ def convert_to_patches(test_x, test_y, test_y_mask, filenames_test,
     patched_labels = []
     patched_masks = []
     total_patches = 0
+
+            
+    # Ensure mask is 2D (H, W)
+    ## basically add some dummy dimensions 
+    if mask.ndim == 3:
+        mask = mask.squeeze()  # Remove extra dimensions
+    elif mask.ndim == 4:
+        mask = mask[0, 0, :, :]  # Extract (H, W) from (1, 1, H, W)
     
+
     for img_idx, (data, label, mask, filename) in enumerate(zip(test_x, test_y, test_y_mask, filenames_test)):
         _, C, H, W = data.shape
         
